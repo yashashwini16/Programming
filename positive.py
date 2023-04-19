847d6ad9f35003ddd59e20f7ee70c50630a6047e
yashashwini16 <yashaswinib1603@gmail.com>
2023-04-20 05:24:19 +0530

    check pos or neg

---
+++ positive.py
@@ -0,0 +1,15 @@
+def pos(n):
+    if n<0:
+        print("it is negative number")
+    elif n>0:
+        print("positive number")
+    else:
+        print("zero")
+
+
+print("enter a number:")
+n=int(input())
+pos(n)
+
+#time complexity:O(1)
+#SPACE   :O(1)
