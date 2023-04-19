233a601b649d7517ca1f16c1121502b29b8d6ae5
yashashwini16 <yashaswinib1603@gmail.com>
2023-04-19 20:40:06 +0530

    factorial

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    """This is a recursive function
+    to find the factorial of an integer"""
+
+    if n == 1 or n==0:
+        return 1
+    else:
+        # recursive call to the function
+        return (n * factorial(n-1))
+num=3  
+result=factorial(num)
+print(result)
