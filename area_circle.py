ec0b3caec4421a11f56553c877e6ac31d337b448
yashashwini16 <yashaswinib1603@gmail.com>
2023-04-20 05:40:02 +0530

    area of rectangle,triangle

---
+++ area_circle.py
@@ -0,0 +1,11 @@
+def circle(r):
+    if r<=0:
+        print("enter a valid area")
+    else:
+        a=2*3.14*r
+        print(a)
+
+
+circle(3)
+
+#to find area of circle
---
+++ area_rect.py
@@ -0,0 +1,17 @@
+def rectangle(l,b):
+        a=l*b
+        if a<=0:
+                print("invalid")
+        else:
+                print(a)
+
+rectangle(2,3)
+rectangle(-2,4)
+#area of rectangle
+
+def triangle(b,h):
+        y=0.5*b*h
+        print(y)
+
+print("area of triangle is:")
+triangle(2,3)
