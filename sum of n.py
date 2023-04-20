e51683de7261d67fbfc855bcf779275ea3c7f076
yashashwini16 <yashaswinib1603@gmail.com>
2023-04-20 20:21:33 +0530

    for

--- sum of n.py
+++ sum of n.py
@@ -9,3 +9,13 @@ n=16
 print(sumofN(n))
 
 #to print sum of first N natural numbers
+
+
+#Write a program to find Sum of numbers in a given range
+def sum(n1,n2):
+    s=0
+    for i in range(n1,n2+1):#range
+        s=s+i
+    print(s)
+
+sum(2,6)
